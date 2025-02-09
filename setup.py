f0cd317a8666cf1bfd609d9703db6da1dce4e952
Mathias Hall-Andersen <mathias@hall-andersen.dk>
2025-02-10 00:26:02 +0100

    build: don't manually set platform tag

--- setup.py
+++ setup.py
@@ -18,31 +18,10 @@ LINUX_TARGET = 'linux'
 DIR_BUILD = 'build'
 DIR_DEST = PACKAGE
 
-def tag():
-    machine = pf.machine()
-    platform = sys.platform
-    if platform == 'darwin':
-        if machine == 'x86_64':
-            return 'macosx_11_0_x86_64'
-        elif machine == 'arm64':
-            return 'macosx_11_0_arm64'
-    elif platform.startswith('linux'):
-        if machine == 'aarch64':
-            return 'linux_aarch64'
-        elif machine == 'x86_64':
-            return 'linux_x86_64'
-    raise ValueError(f"Unsupported machine type: {machine}")
-
 class CustomBdistWheel(bdist_wheel):
     def finalize_options(self):
         super().finalize_options()
         self.root_is_pure = False
-        self.plat_name_supplied = True
-        self.plat_name = tag()
-
-    def get_tag(self):
-        python, abi, _plat = super().get_tag()
-        return python, abi, tag()
 
 setup(
     name=PACKAGE,

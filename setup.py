9353e48dfcbde53f054eaaa1e48eb0ccefd54432
Mathias Hall-Andersen <mathias@hall-andersen.dk>
2025-02-16 15:23:32 +0100

    misc: increase version number

--- setup.py
+++ setup.py
@@ -4,19 +4,7 @@ from wheel.bdist_wheel import bdist_wheel
 from setuptools import setup, find_packages
 
 PACKAGE = 'flatn'
-VERSION = '0.1.2'
-MACOS_FILES = [
-    ('flatter-darwin', 'flatter'),
-    ('libflatter.dylib', 'libflatter.dylib'),
-]
-MACOS_TARGET = 'darwin'
-LINUX_FILES = [
-    ('flatter-linux', 'flatter'),
-    ('libflatter.so', 'libflatter.so'),
-]
-LINUX_TARGET = 'linux'
-DIR_BUILD = 'build'
-DIR_DEST = PACKAGE
+VERSION = '0.2.0'
 
 class CustomBdistWheel(bdist_wheel):
     def finalize_options(self):
